d21db8a4eff778b7b594d87389137f7dd6c3e4aa
Brayhan Holguin <54687783+bholguin@users.noreply.github.com>
2023-07-26 21:32:00 -0500

    modulo y submodulo

--- app/modulos/submodulo/models.py
+++ app/modulos/submodulo/models.py
@@ -4,6 +4,7 @@ class Submodulo(db.Model, BaseModel):
     __tablename__ = 'submodulos'
     id = db.Column(db.String(20), primary_key=True)
     go = db.Column(db.String(50))
+    tipo_doc = db.Column(db.Boolean)
     modulo_id = db.Column(db.String(20), db.ForeignKey("modulos.id"), nullable=False)
 
     def __init__():

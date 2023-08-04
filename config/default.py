cdefce68e8c241699e3c06a27157a580c01ee9c7
Brayhan Holguin <54687783+bholguin@users.noreply.github.com>
2023-08-03 23:08:47 -0500

    crear base de datos

--- config/default.py
+++ config/default.py
@@ -2,13 +2,14 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     # Para propagar las excepciones y poder manejarlas a nivel de aplicación.
     PROPAGATE_EXCEPTIONS = True
-    #Deshabilita las sugerencias de otros endpoints relacionados con alguno que no exista (Flask-Restful).
+    # Deshabilita las sugerencias de otros endpoints relacionados con alguno que no exista (Flask-Restful).
     ERROR_404_HELP = False
     FLASKY_ADMIN = 'Brayhan Holguin'
-    #Configuracion JWT-Extended
+    # Configuracion JWT-Extended
     JWT_ACCESS_TOKEN_EXPIRES = 3600
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
@@ -19,31 +20,32 @@ class Config:
     def init_app(app):
         pass
 
-class DevelopmentConfig(Config):
-    #habilita debug
-    DEBUG = True
-    # Lo utilizan Flask y ciertas extensiones que manejan aspectos de seguridad.
-    SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
-    # Database configuration
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://master:inventario123!@stock-master-database.mysql.database.azure.com:3306/app'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:App1234!@host.docker.internal:3306/app'
-    #Se desactiva como indica la documentación.
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #Se deshabilitan los mensajes de log de SQLAlchemy.
-    SHOW_SQLALCHEMY_LOG_MESSAGES = True
-    
 
-class ProductionConfig(Config):
-    #habilita debug
+class DevelopmentConfig(Config):
+    # habilita debug
     DEBUG = True
     # Lo utilizan Flask y ciertas extensiones que manejan aspectos de seguridad.
     SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
     # Database configuration
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:App1234!@127.0.0.1:3306/app'
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:App1234!@host.docker.internal:3306/app'
-    #Se desactiva como indica la documentación.
+    # Se desactiva como indica la documentación.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #Se deshabilitan los mensajes de log de SQLAlchemy.
+    # Se deshabilitan los mensajes de log de SQLAlchemy.
+    SHOW_SQLALCHEMY_LOG_MESSAGES = True
+
+
+class ProductionConfig(Config):
+    # habilita debug
+    DEBUG = True
+    # Lo utilizan Flask y ciertas extensiones que manejan aspectos de seguridad.
+    SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
+    # Database configuration
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://master:inventario123!@stock-master-database.mysql.database.azure.com:3306/app'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:App1234!@host.docker.internal:3306/app'
+    # Se desactiva como indica la documentación.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Se deshabilitan los mensajes de log de SQLAlchemy.
     SHOW_SQLALCHEMY_LOG_MESSAGES = True
 
 
@@ -52,8 +54,8 @@ class TestingConfig(Config):
 
 
 config = {
- 'development': DevelopmentConfig,
- 'testing': TestingConfig,
- 'production': ProductionConfig,
- 'default': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
 }
